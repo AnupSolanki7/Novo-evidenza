7f1f8a4efd8bc2b34afa02840f5d374f2bd5c785
AnupWiseSkulls7 <anup@wiseskulls.com>
2024-05-28 01:05:35 +0530

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -38,49 +38,70 @@ export default function Home() {
           <h1
             className={title({
               size: "sm",
-              className: "text-gray-600 text-center !leading-[50px]",
+              className: "text-gray-600 text-center !text-xl md:!text-4xl ",
             })}
           >
-            <p className={title({ color: "cyan", size: "sm" })}>
-              Novo Evidenza &nbsp;
-            </p>
-            is registered LLP and established in 2024, Co Founder of Novo
-            Evidenza is{" "}
-            <p className={title({ color: "violet", size: "sm" })}>
-              Dr. Kunal Maheswari
+            Check out our services for your{" "}
+            <p className={title({ color: "blue", className:"!text-xl md:!text-4xl"})}>
+              HEOR solutions &nbsp;
             </p>{" "}
-            , the organization goal is to serve healthcare domain and help
-            pharmaceutical, biotechnology, research organization, Institute for
-            HEOR services, Medical Writing, PV , Regulatory, Medical scribe.
+            / <p className={title({ color: "blue", className:"!text-xl md:!text-4xl"})}>PV &nbsp;</p>/ &nbsp;
+            <p className={title({ color: "blue", className:"!text-xl md:!text-4xl"})}>
+              Regulatory &nbsp;
+            </p>
+            /{" "}
+            <p className={title({ color: "blue", className:"!text-xl md:!text-4xl"})}>
+              Medical Writing &nbsp;
+            </p>{" "}
+            /{" "}
+            <p className={title({ color: "blue", className:"!text-xl md:!text-4xl"})}>
+              Medical Scribe &nbsp;
+            </p>
+            , we have a team of experts in these domains
           </h1>
         </section>
       </Page>
       <Page
         style={{
-          background: "linear-gradient(to bottom, lightblue, white)"
+          background: "linear-gradient(to bottom, lightblue, white)",
         }}
         className="relative h-max"
       >
-        <section className="flex my-auto h-max max-w-[1220px] mx-auto items-center justify-center gap-4 py-8 md:py-10">
-        <Image src={'https://marksmanhealthcare.com/wp-content/uploads/2023/03/MM_-12.png'} className="" width={500} height={500} alt="image" />
-          <h1
-            className={title({
-              size: "sm",
-              className: "text-gray-600 text-center !leading-[50px]",
-            })}
-          >
-            <p className={title({ color: "cyan", size: "sm" })}>
-              Novo Evidenza &nbsp;
+        <section className="flex text-center md:text-left flex-col md:flex-row my-auto h-max max-w-[1220px] mx-auto items-center justify-around gap-4 py-8 md:py-10">
+          <Image
+            src={
+              "https://marksmanhealthcare.com/wp-content/uploads/2023/03/MM_-12.png"
+            }
+            className="md:w-1/4 w-1/2"
+            width={500}
+            height={500}
+            alt="image"
+          />
+          <div className="md:w-1/2 w-full">
+            <p className="font-semibold text-xl text-[#0078A1] mb-4">
+              Our Company
             </p>
-            is registered LLP and established in 2024, Co Founder of Novo
-            Evidenza is{" "}
-            <p className={title({ color: "violet", size: "sm" })}>
-              Dr. Kunal Maheswari
-            </p>{" "}
-            , the organization goal is to serve healthcare domain and help
-            pharmaceutical, biotechnology, research organization, Institute for
-            HEOR services, Medical Writing, PV , Regulatory, Medical scribe.
-          </h1>
+            <h2
+              className={title({
+                size: "sm",
+                className:
+                  "text-gray-600 !font-normal text-center !text-2xl !leading-[35px] ",
+              })}
+            >
+              <p className={title({ color: "cyan", className: "!text-2xl" })}>
+                Novo Evidenza &nbsp;
+              </p>
+              is registered LLP and established in 2024, Co Founder of Novo
+              Evidenza is{" "}
+              <p className={title({ color: "violet", className: "!text-2xl" })}>
+                Dr. Kunal Maheswari
+              </p>{" "}
+              , the organization goal is to serve healthcare domain and help
+              pharmaceutical, biotechnology, research organization, Institute
+              for HEOR services, Medical Writing, PV , Regulatory, Medical
+              scribe.
+            </h2>
+          </div>
         </section>
       </Page>
     </>
